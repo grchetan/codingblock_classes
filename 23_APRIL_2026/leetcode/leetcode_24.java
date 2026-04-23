8d71a9e0f14ae5dbce6e08df89e75f2cdcae9c41
grchetan <chetanprajapat340@gmail.com>
2026-04-23 20:11:35 +0530

    add leetcode quetion 23 april 2026

---
+++ 23_APRIL_2026/leetcode/leetcode_24.java
@@ -0,0 +1,6 @@
+package 23_APRIL_2026.leetcode;
+
+public class leetcode_24 {
+  
+  
+}
---
+++ 23_APRIL_2026/leetcode/leetcode_424.java
@@ -0,0 +1,40 @@
+package _23_APRIL_2026.leetcode;
+
+import java.util.*;
+
+public class leetcode_24 {
+
+    public static int characterReplacement(String s, int k) {
+
+        int[] freq = new int[26];
+        int left = 0;
+        int maxCount = 0;
+        int maxLen = 0;
+
+        for (int right = 0; right < s.length(); right++) {
+
+            freq[s.charAt(right) - 'A']++;
+            maxCount = Math.max(maxCount, freq[s.charAt(right) - 'A']);
+
+            // window shrink
+            while ((right - left + 1) - maxCount > k) {
+                freq[s.charAt(left) - 'A']--;
+                left++;
+            }
+
+            maxLen = Math.max(maxLen, right - left + 1);
+        }
+
+        return maxLen;
+    }
+
+    public static void main(String[] args) {
+
+        String s = "AABABBA";
+        int k = 1;
+
+        int result = characterReplacement(s, k);
+
+        System.out.println("Answer: " + result);
+    }
+}
